f1e2bf6b76821afd7d0299f5df629aa72efcfa8e
Pradeep <pradeepcpk1997@gmail.com>
2024-05-23 22:02:27 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <portal/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import portal from './components/portal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    portal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app2 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2aa74b;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/portal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>I am </h4>
+    <teleport to="#app2">
+      Portal component
+      </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+name:'portal-vue'
+}
+</script>
+
+<style>
+
+</style>
